735baf5167793301db284839ab4dfd0d3671b3b8
35xxv <taiga.090211@gmail.com>
2022-05-14 11:44:44 +0000

    first commit

---
+++ db/migrate/20220425061622_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+    def change
+        create_table :items do |t|
+            t.string :title
+            t.integer :price
+            t.timestamps null: false
+        end
+    end
+end
+
